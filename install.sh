946249f59b1031a0748c1c74409a5d15ab5bf49b
unknown <abolfazlmajidi100@gmail.com>
2025-02-05 14:40:37 +0330

    fix radcli conf

--- install.sh
+++ install.sh
@@ -94,7 +94,7 @@ systemctl stop ocserv
 curl -4 https://raw.githubusercontent.com/imafaz/ocserv/main/ocserv.conf -o /etc/ocserv/ocserv.conf
 sed -i "s/ocserv_port/$ocserv_port/g" /etc/ocserv/ocserv.conf
 curl -4 https://raw.githubusercontent.com/imafaz/ocserv/main/radiusclient.conf -o /etc/radcli/radiusclient.conf
-sed -i "s/ibsng_ip/$ibsng_ip/g" /etc/ocserv/ocserv.conf
+sed -i "s/ibsng_ip/$ibsng_ip/g" /etc/radcli/radiusclient.conf
 curl -4 https://raw.githubusercontent.com/imafaz/ocserv/main/servers -o /etc/radcli/servers
 sed -i "s/ibsng_ip/$ibsng_ip/g" /etc/radcli/servers
 sed -i "s/ibsng_secret/$ibsng_secret/g" /etc/radcli/servers
